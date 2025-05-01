10aab335bbf7b6d304366a4faaee678118ad2152
Khoa1896 <nguyendangkhoa1896@gmail.com>
2025-05-01 19:20:05 +0700

    dd

--- docker-startup.sh
+++ docker-startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sleep 10s
-php artisan key:generate --force --no-interaction
+#php artisan key:generate --force --no-interaction
 php artisan migrate --seed --force --no-interaction
 php artisan storage:link --force --no-interaction
 php artisan serve --host=0.0.0.0 --port=8002
