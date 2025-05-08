5f15ee905164b059e6381526dc2801ccf1304a3f
Khoa1896 <nguyendangkhoa1896@gmail.com>
2025-05-08 21:54:28 +0700

    update sale pos

---
+++ database/migrations/2025_05_08_211147_add_paid_and_purchase_count_to_customers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->decimal('paid', 10, 2)->default(0);
+            $table->decimal('purchase_count')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->dropColumn('paid');
+            $table->dropColumn('purchase_count');
+        });
+    }
+};
